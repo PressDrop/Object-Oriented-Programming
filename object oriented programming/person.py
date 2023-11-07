02bec49a95e9b233af94a8ed855574025dd87a8b
HyperDrop <83242628+PressDrop@users.noreply.github.com>
2023-11-07 14:49:45 +0100

    Delete object oriented programming directory

--- object oriented programming/person.py
+++
@@ -1,5 +0,0 @@
-class Person:
-    def __init__(self, fornavn: str, etternavn:str, telefonnummer:int):
-        self.fornavn = fornavn
-        self.etternavn = etternavn
-        self.telefonnummer = telefonnummer
